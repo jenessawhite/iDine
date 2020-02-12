f31018b3be62c2e149488e11628f3d7569b3e9b8
Jenessa White <jenessaw31@gmail.com>
2020-02-11 23:35:30 -0600

    added grouped listing styles

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -9,25 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(menu) { section in
-                    Text(section.name)
-                        .font(.headline)
-                    ForEach(section.items) { item in
-                        Text(item.name).font(.subheadline)
-                    }
-                }
+  let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+  var body: some View {
+    NavigationView {
+        List {
+          ForEach(menu) { section in
+            Section(header: Text(section.name)) {
+              
+              ForEach(section.items) { item in
+                  Text(item.name).font(.subheadline)
+              }
             }
-        .navigationBarTitle("Our Menu")
+          }
         }
+      .navigationBarTitle("Our Menu")
+      .listStyle(GroupedListStyle())
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
