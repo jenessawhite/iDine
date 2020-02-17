2d5150e54069adf0ee1e5fec12297131fd06b7ce
Jenessa White <jenessaw31@gmail.com>
2020-02-16 23:09:21 -0600

    add the order environment variable

---
+++ iDine/OrderView.swift
@@ -0,0 +1,23 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Jenessa White on 2/16/20.
+//  Copyright © 2020 Jenessa White. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+    }
+}
