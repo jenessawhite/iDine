940c672b29a34c4f01bee0048858454e356e1443
Jenessa White <jenessaw31@gmail.com>
2020-02-16 23:12:30 -0600

    finish stubbing out order view

--- iDine/OrderView.swift
+++ iDine/OrderView.swift
@@ -12,12 +12,34 @@ struct OrderView: View {
     @EnvironmentObject var order: Order
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+
+                Section {
+                    NavigationLink(destination: Text("Check out")) {
+                        Text("Place Order")
+                    }
+                }
+            }
+            .navigationBarTitle("Order")
+            .listStyle(GroupedListStyle())
+        }
     }
 }
 
 struct OrderView_Previews: PreviewProvider {
+    static let order = Order()
+
     static var previews: some View {
-        OrderView()
+        OrderView().environmentObject(order)
     }
 }
